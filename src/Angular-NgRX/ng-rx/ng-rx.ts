43dd211c30b0d47cf0b1ed3e7f10eb16b735ac62
ljojua <ljojua@crocobet.com>
2026-01-12 17:35:54 +0400

    feat: set up project

---
+++ src/Angular-NgRX/ng-rx/ng-rx.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'weather-ng-rx',
+  imports: [],
+  templateUrl: './ng-rx.html',
+  styleUrl: './ng-rx.scss',
+})
+export class NgRx {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,13 @@
 import { Component, signal } from '@angular/core';
-import {Layout} from './components/layout/layout';
-import {RouterOutlet} from '@angular/router';
-
+import { Layout } from './components/layout/layout';
+import { RouterOutlet } from '@angular/router';
+import { NgRx } from '../Angular-NgRX/ng-rx/ng-rx';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet
-  ],
+  imports: [NgRx],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('weather');
