b9cf58684729a503702a8572ab259ef31020e18a
ljojua <ljojua@crocobet.com>
2026-01-13 20:01:47 +0400

    feat: lesson 2

---
+++ src/Angular-NgRX/ng-pages/students-table/students-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'weather-students-table',
+  imports: [],
+  templateUrl: './students-table.html',
+  styleUrl: './students-table.scss',
+})
+export class StudentsTable {
+
+}
